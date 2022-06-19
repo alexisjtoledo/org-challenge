01acb49ba9b9ce713a40444753bd52bbff46ea32
Alexis Toledo <alexis.toledo@live.com>
2022-06-19 17:42:06 +0200

    [FEAT] Adds hero section

--- src/components/HeroSection/HeroSection.styles.tsx
+++ src/components/HeroSection/HeroSection.styles.tsx
@@ -1,3 +1,154 @@
 import styled from 'styled-components';
+import { getFont, getFontSize } from '@app/styles/theme/font';
+import { getColor } from '@app/styles/theme/colors';
+import { media } from '@app/styles/media';
+import MaxWidthContainer from '../MaxWidthContainer/MaxWidthContainer';
+import Link from '../Link/Link';
 
-export const StyledHeroSectionContainer = styled.section``;
+export const StyledHeroSectionContainer = styled(MaxWidthContainer)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 85vw;
+  height: auto;
+  margin: 46px 7.5vw !important;
+  padding: 0 !important;
+
+  ${media.sm`
+    width: 66vw;
+    margin: 56px 17vw !important;
+  `}
+
+  ${media.lg`
+    flex-direction: row-reverse;
+    margin: 90px auto !important; 
+  `}
+`;
+
+export const StyledHeadlineContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 40px;
+
+  ${media.sm`
+    margin-top: 56px;
+  `}
+
+  ${media.lg`
+    max-width: 50%;
+    margin-top: 0;
+  `}
+`;
+
+export const StyledHeadlineTitle = styled.h1`
+  font-family: ${getFont('primary')};
+  line-height: 56px;
+  font-size: 44px;
+  text-align: center;
+  font-weight: 700;
+  margin-block-start: 0;
+  margin-block-end: 0;
+
+  ${media.sm`
+    font-size: 66px;
+    line-heght: 72px;
+  `}
+
+  ${media.lg`
+    line-height: 66px;
+    font-size: 72px;
+    text-align: left;
+  `}
+`;
+
+export const StyledHeadlineCopy = styled.p`
+  font-family: ${getFont('primary')};
+  font-size: 26px;
+  font-weight: 400;
+  text-align: center;
+  line-height: 40px;
+  color: #767676;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  margin-top: 16px;
+
+  ${media.sm`
+    font-size: 26px;
+    line-height: 34px;
+  `}
+
+  ${media.lg`
+    line-height: 34px;
+    font-size: 34px;
+    text-align: left;
+  `}
+`;
+
+export const StyledActionsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 32px;
+
+  ${media.sm`
+    flex-direction: row;
+  `}
+`;
+
+export const StyledCTAButton = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 56px;
+  transition: all 100ms ease;
+  box-sizing: border-box;
+  border-radius: 10px;
+  font-size: ${getFontSize('fs5')};
+  font-weight: 400;
+  white-space: nowrap;
+  margin-bottom: 10px;
+  background-color: ${getColor('primary')('s100')};
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+    color: white !important;
+  }
+
+  ${media.sm`
+    margin-bottom: 0px;
+    margin-right: 10px;
+  `}
+`;
+
+export const StyledCTALink = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 56px;
+  font-size: ${getFontSize('fs5')};
+  font-weight: 400;
+  white-space: nowrap;
+  cursor: pointer;
+`;
+
+export const StyledImgContainer = styled.div`
+  width: 100%;
+  height: auto;
+
+  ${media.sm`
+    width: 85%;
+  `}
+
+  ${media.lg`
+    width: 40%;
+  `}
+`;
+
+export const StyledImg = styled.img`
+  width: 100%;
+  height: 100%;
+`;
--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import { StyledHeroSectionContainer } from './HeroSection.styles';
+import {
+  StyledHeroSectionContainer,
+  StyledImgContainer,
+  StyledImg,
+  StyledHeadlineContainer,
+  StyledHeadlineTitle,
+  StyledHeadlineCopy,
+  StyledActionsContainer,
+  StyledCTAButton,
+  StyledCTALink,
+} from './HeroSection.styles';
+import HeroImg from '../../routes/demo/assets/sectionOne@3x.jpg';
 
 const HeroSection = (): JSX.Element => (
-  <StyledHeroSectionContainer>
-    <p>Hero Section</p>
+  <StyledHeroSectionContainer role="section">
+    <StyledImgContainer>
+      <StyledImg
+        src={HeroImg}
+        alt="Example of organigram. First level: Peter as CEO, second level: Peter as VP Product and Sarah as (CTO), third level: Emilie as Senior Product Designer (highlighted as just joined)."
+      />
+    </StyledImgContainer>
+    <StyledHeadlineContainer>
+      <StyledHeadlineTitle>Transparency starts here</StyledHeadlineTitle>
+      <StyledHeadlineCopy>
+        Join the world&apos;s biggest professional community for teams
+      </StyledHeadlineCopy>
+      <StyledActionsContainer>
+        <StyledCTAButton href="/get-started">Get started</StyledCTAButton>
+        <StyledCTALink href="/about">Why The Org?</StyledCTALink>
+      </StyledActionsContainer>
+    </StyledHeadlineContainer>
   </StyledHeroSectionContainer>
 );
 
